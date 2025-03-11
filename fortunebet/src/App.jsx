b111e5de5f58a4747eb3f6ad1bccd55594b2ae11
owen <owensmith31588@gmail.com>
2025-03-12 00:56:01 +0300

    initialize React app with Vite and Tailwind CSS setup

---
+++ fortunebet/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center text-red-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default App;
